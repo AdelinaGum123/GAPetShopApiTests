300ffe38a7156d9c46b98e29c25cf1104466f786
AdelinaGum123 <gumerovarths@gmail.com>
2025-05-31 12:07:16 +0300

    Добавила тест "Попытка получить информацию о несуществуещем питомце"

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -33,6 +33,21 @@ class TestPet:
             assert response.text == "Pet not found", "Текст ошибки не совпал с ожидаемым"
 
 
+    @allure.title("Попытка получить информацию о несуществуещем питомце")
+    def test_get_information_nonexistent_pet(self):
+        with allure.step("Отправка запроса на получение информации о несуществуещем питомце"):
+            response = requests.get(url=f"{BASE_URL}/pet/9999")
+
+        with allure.step("Проверка статуса ответа"):
+            assert response.status_code == 404, "Код ответа не совпал с ожидаемым"
+
+        with allure.step("Проверка текста ответа"):
+            assert response.text == "Pet not found", "Текст ошибки не совпал с ожидаемым"
+
+
+
+
+
 
 
 
